706cfcc33d7b835e6bc381e0c7d04f3cbd223592
serenazunino <serena.zunino@gmail.com>
2020-10-26 17:35:42 +0100

    leonor->tamara

--- R/setup.R
+++ R/setup.R
@@ -33,9 +33,10 @@ rastFolder <- file.path("C:/Users/szunino/Documents/github/Atl_ohi/prep/FIS/annu
 atl_gh<-"https://github.com/serenzun/Atl_ohi"
 atl_ohi<-file.path("C:/Users/szunino/Documents/github/Atl_ohi/prep/FIS/annual_catch/")
 
-<<<<<<< HEAD
-##set the memory size (since I'm working on the virtual machine)
-memory.limit(size=56000)
-=======
-#Spatial files
->>>>>>> 34356ef819cd2f03894c741cae40a1cba0dc658c
+#<<<<<<< HEAD
+###set the memory size (since I'm working on the virtual machine)
+#memory.limit(size=56000)
+#=======
+##Spatial files
+#>>>>>>> 34356ef819cd2f03894c741cae40a1cba0dc658c
+#
---
+++ prep/spatial/README.md
@@ -0,0 +1,13 @@
+##Spatial script to create shp and raster file
+
+*Output*
+atl_mask.tiff : raster file to use to crop all the other file. It has been created by merging the FAO shaprfile of the Atlantic Ocean
+
+
+
+
+Steps:
+-create raster mask for the Atlantic Ocean
+-creata a file with regions ID for OHI, FAO and Longhurst ecosystems. This script will be useful when the Atlanteco rpoject will defined the spatial boundaries of teh the study.
+
+
