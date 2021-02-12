1511108800613d87bf0c577b605f764f0750eee8
Serena Zunino <szunino0@r034c01s01.galileo.cineca.it>
2021-02-12 18:14:30 +0100

    please work

---
+++ R/directories_galileo.R
@@ -0,0 +1,11 @@
+##Directories:
+#dir_prep<-here::here("prep")
+dir_B<-file.path ("/galileo/home/userexternal/szunino0/big") #virtual machine
+dir_sp<-file.path("C:/USers/Acer/Documents/big/prep/spatial")
+#rastFolder <- file.path("C:/Users/szunino/Documents/github/Atl_ohi/prep/FIS/annual_catch/")
+rastFolder<-"C:/Users/Acer/Documents/big/prep/fis/watson/raw/raster/"#non ci capisco piu nulla. check readme fis_prs
+atl_gh<-"https://github.com/serenzun/Atl_ohi"
+atl_ohi<-file.path("C:/Users/Acer/Documents/github/Atl_ohi/prep/FIS/annual_catch/")
+
+
+data_wd<-file.path("/galileo/home/userexternal/szunino0/big/R_raster_tmp/trash")
